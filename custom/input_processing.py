3bab283f30515db36910dbb1b34bf4d3d42455a9
Milkhail <0001@granin.com>
2023-04-22 12:52:17 +0300

    input module works but in wrong folder : # custom/__init__.py - Initializes the 'custom' module for user input processing.
    # chatgpt_telegram_bot/custom/__init__.py - Initializes the 'custom' module for the chatgpt_telegram_bot.
    
    # chatgpt_telegram_bot/custom/input_capture.py - Demonstrates the use of the input_processing module for capturing user input.
    
    # custom/input_processing.py - Contains the process_input function for sanitizing and preparing user input for further processing.
    
    # chatgpt_telegram_bot/custom/main.py - Demonstrates the use of the input_processing module in a main application.
    
    # tests/unit/test_input_processing.py - Contains unit tests for the process_input function in the input_processing module.

---
+++ custom/input_processing.py
@@ -0,0 +1,6 @@
+# custom/input_processing.py - Contains the process_input function for sanitizing and preparing user input for further processing.
+
+def process_input(user_input):
+    # Sanitize and prepare the input for further processing
+    sanitized_input = user_input.strip()
+    return sanitized_input
---
+++ tests/unit/test_input_processing.py
@@ -0,0 +1,23 @@
+
+# tests/unit/test_input_processing.py - Contains unit tests for the process_input function in the input_processing module.
+
+
+import unittest
+from custom.input_processing import process_input
+
+
+class TestInputProcessing(unittest.TestCase):
+
+    def test_process_input_valid(self):
+        self.assertEqual(process_input("hello"), "hello")
+        self.assertEqual(process_input("  hello world  "), "hello world")
+
+    def test_process_input_invalid(self):
+        self.assertNotEqual(process_input("  hello world  "), "  hello world  ")
+
+    def test_process_input_empty(self):
+        self.assertEqual(process_input("  "), "")
+        self.assertEqual(process_input(""), "")
+
+if __name__ == '__main__':
+    unittest.main()
