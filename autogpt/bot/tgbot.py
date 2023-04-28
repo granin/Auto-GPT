45f1312aab919d79746056d6dd8ec4f4fb86b9bd
Milkhail <0001@granin.com>
2023-04-28 08:33:16 +0300

    some output to telegram but wrong and on starat before actual tool is running

--- autogpt/bot/tgbot.py
+++ autogpt/bot/tgbot.py
@@ -1,13 +1,48 @@
-
+import os
+import time
+import threading
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import asyncio
 
-def on_output_file_updated(content):
-    global chat_id, bot_instance
+def get_file_mtime(filename):
+    return os.stat(filename).st_mtime
+
+def monitor_file_changes(context):
+    output_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'Assistant_Reply.txt')
+    input_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'Processed_Input.txt')
+
+    last_mtime = None
+    last_content = None
+    user_input_expected = False
+
+    while True:
+        if os.path.exists(output_filename):
+            current_mtime = get_file_mtime(output_filename)
+
+            if last_mtime is None or current_mtime != last_mtime:
+                with open(output_filename, "r") as f:
+                    content = f.read()
+                    if content != last_content and content.strip() != "":
+                        user_input_expected = on_output_file_updated(content, context)
+                        last_content = content
+                        last_mtime = current_mtime
+                    else:
+                        user_input_expected = False
+
+        else:
+            last_mtime = None
+
+        time.sleep(0.5)
+
+
+def on_output_file_updated(content, context):
+    global chat_id
     if chat_id:
-        bot_instance.send_message(chat_id=chat_id, text=f"File updated: {content}")
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(context.bot.send_message(chat_id=chat_id, text=f"File updated: {content}"))
     return True
-
 def get_user_input():
     return "y"  # This will be replaced by the Telegram command
 
@@ -26,6 +61,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global chat_id
     chat_id = update.message.chat_id
     await update.message.reply_text('Monitoring started')
+
+    monitor_thread = threading.Thread(target=monitor_file_changes, args=(context,))
+    monitor_thread.start()
+
 
 app = ApplicationBuilder().token("6253259092:AAG6bPFPOEbo5WOcTcXrbs-S_RwtZBM7jKQ").build()
 app.add_handler(CommandHandler("hello", hello))
