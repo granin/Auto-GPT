813a91d604d9772ea48b4ffe2ce09990a5ad01af
Milkhail <0001@granin.com>
2023-05-03 18:54:04 +0300

    start all and restart all works file

--- autogpt/bot/tgbot.py
+++ autogpt/bot/tgbot.py
@@ -6,30 +6,27 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 import asyncio
 import yaml
 
+import os
+import platform
+
 from capture_utils import get_human_feedback
 from bot import get_file_mtime, rename_old_session_files
 import subprocess
 
 def write_ai_settings():
-    ai_settings = {
-        "ai_name": "AI Name",
-        "ai_role": "AI Role",
-        "ai_goals": ["Goal 1", "Goal 2", "Goal 3"],
-    }
+    global ai_config_values
 
     ai_settings_path = "/Users/m/git/1ai/Auto-GPT/ai_settings.yaml"
 
     with open(ai_settings_path, "w") as outfile:
-        outfile.write("ai_name: {}\n".format(ai_settings["ai_name"]))
-        outfile.write("ai_role: {}\n".format(ai_settings["ai_role"]))
+        outfile.write("ai_name: {}\n".format(ai_config_values["name"]))
+        outfile.write("ai_role: {}\n".format(ai_config_values["role"]))
         outfile.write("ai_goals:\n")
-        for goal in ai_settings["ai_goals"]:
+        for goal in ai_config_values["goals"]:
             outfile.write("- '{}'\n".format(goal))
 
     return ai_settings_path
 
-import os
-import platform
 
 async def start_autogpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global ai_config_values
@@ -122,6 +119,109 @@ async def set_ai_config(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
                                     f"Name: {name}\n"
                                     f"Role: {role}\n"
                                     f"Goals: {', '.join(goals)}")
+async def restart_autogpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    ai_settings_path = "/Users/m/git/1ai/Auto-GPT/ai_settings.yaml"
+
+    if not os.path.exists(ai_settings_path):
+        await update.message.reply_text("AI configuration is not set. Please use /set_ai_config or /start_all command to set the AI configuration.")
+        return
+
+    run_sh_path = "/Users/m/git/1ai/Auto-GPT/run.sh"
+    script_dir = os.path.dirname(run_sh_path)
+    ai_settings_abs_path = os.path.abspath(ai_settings_path)
+    command = f"cd {os.path.dirname(ai_settings_abs_path)} && ./run.sh --ai-settings {ai_settings_abs_path}"
+    system = platform.system()
+    if system == "Darwin" or system == "Linux":
+        os.system(f"gnome-terminal -- bash -c '{command}; exec bash'" if system == "Linux" else f"osascript -e 'tell application \"Terminal\" to do script \"{command}\"'")
+        await update.message.reply_text("Auto-GPT process started in a new terminal.")
+    else:
+        await update.message.reply_text(f"Unsupported platform. Run the following command in a separate terminal:\n\n{command}")
+
+async def restart_all(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Start Auto-GPT
+    await restart_autogpt(update, context)
+
+    # Start monitoring
+    global chat_id
+    chat_id = update.message.chat_id
+    await update.message.reply_text('Monitoring started')
+
+    # Rename old session files
+    if os.path.exists(output_filename):
+        rename_old_session_files(output_filename)
+    if os.path.exists(input_filename):
+        rename_old_session_files(input_filename)
+
+    # Create new empty files for the new session
+    open(output_filename, "w").close()
+    open(input_filename, "w").close()
+
+    # Create the start event
+    start_event = threading.Event()
+    start_event.set()
+
+    bot_event_loop = asyncio.get_event_loop()
+    monitor_thread = threading.Thread(target=monitor_file_changes, args=(context, bot_event_loop, start_event))
+    monitor_thread.start()
+
+# ...
+
+
+async def start_all(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message_text = update.message.text
+    _, name, role, *goals = message_text.split('|')
+
+    if not goals:
+        await update.message.reply_text("Please provide the AI Name, AI Role, and Goals in the format:\n"
+                                        "/start_all | AI Name | AI Role | Goal 1 | Goal 2 | Goal 3")
+        return
+
+    # Set AI config
+    global ai_config_values
+    ai_config_values = {
+        'name': name.strip(),
+        'role': role.strip(),
+        'goals': [goal.strip() for goal in goals]
+    }
+
+    # ... rest of the code
+
+
+    # Start Auto-GPT
+    ai_settings_path = write_ai_settings()
+    run_sh_path = "/Users/m/git/1ai/Auto-GPT/run.sh"
+    script_dir = os.path.dirname(run_sh_path)
+    ai_settings_abs_path = os.path.abspath(ai_settings_path)
+    command = f"cd {os.path.dirname(ai_settings_abs_path)} && ./run.sh --ai-settings {ai_settings_abs_path}"
+    system = platform.system()
+    if system == "Darwin" or system == "Linux":
+        os.system(f"gnome-terminal -- bash -c '{command}; exec bash'" if system == "Linux" else f"osascript -e 'tell application \"Terminal\" to do script \"{command}\"'")
+        await update.message.reply_text("Auto-GPT process started in a new terminal.")
+    else:
+        await update.message.reply_text(f"Unsupported platform. Run the following command in a separate terminal:\n\n{command}")
+
+    # Start monitoring
+    global chat_id
+    chat_id = update.message.chat_id
+    await update.message.reply_text('Monitoring started')
+
+    # Rename old session files
+    if os.path.exists(output_filename):
+        rename_old_session_files(output_filename)
+    if os.path.exists(input_filename):
+        rename_old_session_files(input_filename)
+
+    # Create new empty files for the new session
+    open(output_filename, "w").close()
+    open(input_filename, "w").close()
+
+    # Create the start event
+    start_event = threading.Event()
+    start_event.set()
+
+    bot_event_loop = asyncio.get_event_loop()
+    monitor_thread = threading.Thread(target=monitor_file_changes, args=(context, bot_event_loop, start_event))
+    monitor_thread.start()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global chat_id
@@ -154,5 +254,8 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get_human_feedback))
 app.add_handler(CommandHandler("set_ai_config", set_ai_config))
 app.add_handler(CommandHandler("start_autogpt", start_autogpt))
+app.add_handler(CommandHandler("start_all", start_all))
+app.add_handler(CommandHandler("restart_all", restart_all))
+
 
 app.run_polling()
